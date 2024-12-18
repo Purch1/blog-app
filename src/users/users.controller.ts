0799e8d2988ca49d3ff4f6dfd583712ef86984ba
purch1 <ucheprince26@gmail.com>
2024-12-18 16:18:26 +0100

    Fixed bug and create users files

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Patch, Put, Delete } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get('/:id')
+  public getUsers() {
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUsers() {
+    return 'You sent a post request to users endpoint';
+  }
+}
